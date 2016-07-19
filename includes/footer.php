9a329f856635cdc3a7adc1223b6ace852dd2c150
Brent <puddletowndesign@gmail.com>
2016-07-19 01:28:58 -0700

    Final version 4.4 update

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,4 @@
-	
 <footer>
-    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(2) ) : ?>
-        <?php #dynamic_sidebar('footer-contact') ?>
-    <?php endif; ?>
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(2) ) : ?><?php #dynamic_sidebar('footer-contact') ?><?php endif; ?>
 	&copy; <?php echo date("Y"); ?> <a href="<?php echo site_url(); ?>"><?php bloginfo( 'name' ); ?></a>
 </footer>
